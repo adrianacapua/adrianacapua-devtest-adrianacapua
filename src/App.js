f5d805dbdf5fcc597b64463a124271619a781549
adrianacapua <adrianajcapua@gmail.com>
2022-03-25 00:51:24 +0800

    feat: initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import CardList from "./components/molecule/CardList";
+import Header from "./components/organism/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <Header classname='header' title='Title Task'/>
+      <CardList photos={['test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test', 'test']} />
     </div>
   );
 }
